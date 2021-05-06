6a94e80f8b7a93286119b020a87238e45c0364cc
Ben Kuster <bkuster@virtualcitysystems.de>
2021-05-06 14:27:03 +0200

    updated packages

--- build/postinstall.js
+++ build/postinstall.js
@@ -21,18 +21,8 @@ async function fixTinyQueue() {
   }
 }
 
-async function fixCesium() {
-  const fileName = path.join(process.cwd(), 'node_modules', 'cesium', 'package.json');
-  if (fs.existsSync(fileName)) {
-    const content = await fs.promises.readFile(fileName);
-    const jsonContent = JSON.parse(content);
-    jsonContent.exports['./Source/'] = './Source/';
-    await fs.promises.writeFile(fileName, JSON.stringify(jsonContent, null, 2));
-  }
-}
-
 async function run() {
-  await Promise.all([fixOpenlayers(), fixTinyQueue(), fixCesium()]);
+  await Promise.all([fixOpenlayers(), fixTinyQueue()]);
   console.log('fixed modules');
 }
 
