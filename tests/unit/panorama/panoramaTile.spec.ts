006d901dec32da79391973a0ca6d344a7ac80169
Ben Kuster <bkuster@vc.systems>
2025-01-30 16:21:36 +0100

    fixed distance

--- tests/unit/panorama/panoramaTile.spec.ts
+++ tests/unit/panorama/panoramaTile.spec.ts
@@ -1,6 +1,8 @@
 import { expect } from 'chai';
 import {
+  getDistanceToTileCoordinate,
   getTileCoordinatesInImageExtent,
+  getTileSphericalCenter,
   tileCoordinateFromImageCoordinate,
 } from '../../../src/panorama/panoramaTile.js';
 
@@ -40,6 +42,49 @@ describe.only('Panorama Tile', () => {
         2,
       ).map((tc) => tc.key);
       expect(keys).to.have.lengthOf(6);
+    });
+  });
+
+  describe('getting the center of a tile', () => {
+    it('should get the center of the tile', () => {
+      expect(
+        getTileSphericalCenter({ x: 1, y: 0, level: 0, key: '0/0/0' }),
+      ).to.have.ordered.members([Math.PI / 2, Math.PI / 2]);
+      expect(
+        getTileSphericalCenter({
+          x: 15,
+          y: 4,
+          level: 3,
+          key: '3/15/4',
+        }),
+      ).to.have.ordered.members([Math.PI / 16, Math.PI / 2 + Math.PI / 16]);
+    });
+  });
+
+  describe('calculating a tiles distance to an image coordinate', () => {
+    it('should calculate the distance to the tile', () => {
+      expect(
+        getDistanceToTileCoordinate([Math.PI / 2, Math.PI / 2], {
+          x: 1,
+          y: 0,
+          level: 0,
+          key: '0/0/0',
+        }),
+      ).to.equal(0);
+    });
+
+    it('should calculate the shortest distance around the globe', () => {
+      expect(
+        getDistanceToTileCoordinate(
+          [Math.PI * 2 - Math.PI / 16, Math.PI / 2 + Math.PI / 16],
+          {
+            x: 15,
+            y: 4,
+            level: 3,
+            key: '3/15/4',
+          },
+        ),
+      ).to.be.closeTo(Math.PI / 8, 0.0000000001);
     });
   });
 });
