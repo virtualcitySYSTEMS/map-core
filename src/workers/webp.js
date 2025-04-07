dfd025f1a92ceec23a804841b39d6fbc589f2e0b
Ben Kuster <bkuster@vc.systems>
2025-04-07 11:45:01 +0200

    worker TS

--- src/workers/webp.js
+++
@@ -1,6 +0,0 @@
-self.addEventListener('message', async (e) => {
-  const { id, buffer } = e.data;
-  const blob = new Blob([buffer]);
-  const decoded = await createImageBitmap(blob);
-  self.postMessage({ decoded, id }, [decoded]);
-});
