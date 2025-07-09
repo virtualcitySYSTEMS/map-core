658ed2e42896c8714d84e66ed71d001c67c6826e
Ben Kuster <bkuster@vc.systems>
2025-07-09 10:02:06 +0000

    panorama layers

--- src/layer/panoramaDatasetLayer.ts
+++ src/layer/panoramaDatasetLayer.ts
@@ -1,8 +1,89 @@
-import VectorTileLayer from './vectorTileLayer.js';
+import VectorTileLayer, { type VectorTileImpls } from './vectorTileLayer.js';
 import type PanoramaDataset from '../panorama/panoramaDataset.js';
+import PanoramaDatasetPanoramaImpl from './panorama/panoramaDatasetPanoramaImpl.js';
+import type VcsMap from '../map/vcsMap.js';
+import PanoramaMap from '../map/panoramaMap.js';
+import VectorProperties, { PrimitiveOptionsType } from './vectorProperties.js';
+import { maxZIndexMin50 } from '../util/layerCollection.js';
 
-export default class PanoramaDatasetLayer extends VectorTileLayer {
-  constructor(dataset: PanoramaDataset) {
-    super({ tileProvider: dataset.tileProvider });
+export default class PanoramaDatasetLayer extends VectorTileLayer<PanoramaDatasetPanoramaImpl> {
+  static get className(): string {
+    return 'PanoramaDatasetLayer';
+  }
+
+  private _hideInPanorama = false;
+
+  private _panoramaVectorProperties = new VectorProperties({});
+
+  constructor(public readonly dataset: PanoramaDataset) {
+    const color = 'rgba(255, 255, 255, 0.5)';
+
+    super({
+      tileProvider: dataset.tileProvider,
+      vectorProperties: {
+        altitudeMode: 'absolute',
+        extrudedHeight: 1.8,
+      },
+      style: {
+        type: 'VectorStyleItem',
+        fill: { color },
+        image: {
+          radius: 5,
+          fill: { color },
+          stroke: {
+            color: 'rgb(0, 0, 0)',
+            width: 1,
+          },
+        },
+      },
+      minLevel: 15,
+      maxLevel: 22,
+      zIndex: maxZIndexMin50,
+    });
+
+    this._panoramaVectorProperties = new VectorProperties({
+      altitudeMode: 'absolute',
+      primitiveOptions: {
+        type: PrimitiveOptionsType.CYLINDER,
+        geometryOptions: {
+          topRadius: 1,
+          bottomRadius: 1,
+          length: 0.01,
+        },
+        offset: [0, 0, this.dataset.cameraOffset],
+      },
+    });
+
+    this._supportedMaps.push(PanoramaMap.className);
+  }
+
+  get hideInPanorama(): boolean {
+    return this._hideInPanorama;
+  }
+
+  set hideInPanorama(value: boolean) {
+    if (this._hideInPanorama !== value) {
+      this._hideInPanorama = value;
+      this.getImplementations().forEach((impl) => {
+        if (impl instanceof PanoramaDatasetPanoramaImpl) {
+          impl.hideInPanorama = value;
+        }
+      });
+    }
+  }
+
+  override createImplementationsForMap(
+    map: VcsMap,
+  ): (PanoramaDatasetPanoramaImpl | VectorTileImpls)[] {
+    if (map instanceof PanoramaMap) {
+      return [
+        new PanoramaDatasetPanoramaImpl(map, {
+          ...this.getImplementationOptions(),
+          vectorProperties: this._panoramaVectorProperties,
+          hideInPanorama: this.hideInPanorama,
+        }),
+      ];
+    }
+    return super.createImplementationsForMap(map);
   }
 }
